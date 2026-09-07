804a863cb9c1d4c2cb6cba49b26465e8a15a3735
alibek2672 <alibek.panesh@mail.ru>
2026-09-07 13:47:23 +0300

    Начал разработку

---
+++ logic.py
@@ -0,0 +1,3 @@
+class Logic:
+    def __init__():
+        return 1
---
+++ ui.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+from logic import *
+
+
+__all__ = ["UI"]
+
+class UI(ctk):
+    def __init__():
+        return 1
