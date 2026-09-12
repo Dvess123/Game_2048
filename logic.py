85f6a1e469d0a3a4387e1bb3f3bc0071490041f6
Zlatoi4894 <alibek.panesh@mail.ru>
2026-09-12 16:18:48 +0300

    bug

--- logic.py
+++ logic.py
@@ -9,22 +9,22 @@ class Logic:
             for _ in range(self.board_size)
         ]
 
-        self.matrix[0][0] = 2
-        self.matrix[0][1] = 4
-        self.matrix[0][2] = 2
-        self.matrix[0][3] = 4
+        # self.matrix[0][0] = 2
+        # self.matrix[0][1] = 4
+        # self.matrix[0][2] = 2
+        # self.matrix[0][3] = 4
 
-        self.matrix[1][0] = 4
-        self.matrix[1][1] = 2
-        self.matrix[1][2] = 4
-        self.matrix[1][3] = 2
+        # self.matrix[1][0] = 4
+        # self.matrix[1][1] = 2
+        # self.matrix[1][2] = 4
+        # self.matrix[1][3] = 2
 
-        self.matrix[2][0] = 2
-        self.matrix[2][1] = 4
-        self.matrix[2][2] = 2
-        self.matrix[2][3] = 4
+        # self.matrix[2][0] = 2
+        # self.matrix[2][1] = 4
+        # self.matrix[2][2] = 2
+        # self.matrix[2][3] = 4
 
-        self.matrix[3][0] = 4
+        # self.matrix[3][0] = 4
 
         
 
